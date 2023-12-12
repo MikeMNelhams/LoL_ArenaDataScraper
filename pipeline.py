d019f2c6ad4501105a44f00f5cae13fd9c7d4bab
MikeMNelhams <mikemnelhams@gmail.com>
2023-12-12 22:41:04 +0000

    Changelist:
    - Corrected ALOT of bugs in the recursive game saver

--- pipeline.py
+++ pipeline.py
@@ -19,10 +19,14 @@ def read_api_key() -> str:
     return config["RIOT_DEV_KEY"]
 
 
-def get_summoners_matches(region, summoner_name_seed, watcher):
-    player_data = watcher.summoner.by_name(region, summoner_name_seed)
+def get_summoners_matches(region, summoner_name, watcher, start: int=0, count: int=20):
+    player_data = watcher.summoner.by_name(region, summoner_name)
     puuid = player_data["puuid"]
-    my_matches = watcher.match.matchlist_by_puuid(region, puuid)
+    return get_puuid_matches(region, puuid, watcher, start=start, count=count)
+
+
+def get_puuid_matches(region, puuid, watcher, start: int=0, count: int=20):
+    my_matches = watcher.match.matchlist_by_puuid(region, puuid, start=start, count=count)
     return my_matches
 
 
@@ -130,21 +134,21 @@ def add_new_champ() -> None:
 
 
 def save_matches_recursive(summoner_name_seed: str, region: str = "euw1", num_matches: int=100,
-                           num_matches_to_check_per_player: int=20) -> None:
+                           num_matches_to_check_per_player: int=10) -> None:
     champion_stats_reader = ChampPlacementWriter("champion_placements.csv")
 
     watcher = LolWatcher(read_api_key())
 
-    my_match_ids = get_summoners_matches(region, summoner_name_seed, watcher)
+    my_match_ids = get_summoners_matches(region, summoner_name_seed, watcher, count=num_matches_to_check_per_player)
     my_puuid = watcher.summoner.by_name(region, summoner_name_seed)["puuid"]
 
     i = 1
     player_puuids_checked = {my_puuid}
-    match_ids_to_check = my_match_ids[:num_matches_to_check_per_player]
+    match_ids_to_check = my_match_ids
     match_ids_checked = set(match_ids_to_check)
     recorded_games = set(champion_stats_reader.recorded_games.columns.tolist())
     while i < num_matches or not match_ids_to_check:
-        pop_index = random.randint(0, len(match_ids_to_check) - 1)
+        pop_index = random.randint(0, max(len(match_ids_to_check) - 1, 1))  # Randomised to prevent recency bias
         current_match_id = match_ids_to_check.pop(pop_index)
         match_ids_checked.add(current_match_id)
         match_detail = watcher.match.by_id(region, current_match_id)
@@ -155,14 +159,15 @@ def save_matches_recursive(summoner_name_seed: str, region: str = "euw1", num_ma
 
         time.sleep(3)  # Rate limiting as to not time-out (Max 1 requests per 3 seconds)
         match_puuids = parse_match_puuids(match_detail)
-
         match = Match.from_game_data(match_detail)
 
         for puuid in match_puuids:
             if puuid not in player_puuids_checked:
                 print(f"Found new player puuid: \'{puuid}\'")
                 player_puuids_checked.add(puuid)
-                player_matches = get_summoners_matches(region, summoner_name_seed, watcher)
+                # Even though possible not, it's likely the 0th index match is the match you searched by.
+                player_matches = get_puuid_matches(region, puuid, watcher,
+                                                       start=1, count=num_matches_to_check_per_player)
                 time.sleep(6)  # Rate limiting as to not time-out (Max 2 requests per 6 seconds)
                 for match_id in player_matches:
                     if match_id not in match_ids_checked:
